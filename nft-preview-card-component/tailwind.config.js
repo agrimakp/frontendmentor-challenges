aa9089f22c10956c3950193c758f9b8cd2445211
Agrima <qvistn111@gmail.com>
2021-12-01 20:52:27 +0530

    implemented tailwind css

--- nft-preview-card-component/tailwind.config.js
+++ nft-preview-card-component/tailwind.config.js
@@ -1,14 +1,26 @@
 module.exports = {
-  mode: 'jit',
-  purge: [
-    "./index.html"
-  ],
+  mode: "jit",
+  purge: ["./index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(178, 100%, 50%)",
+        blue: {
+          soft: "hsl(215, 51%, 70%)",
+          dark: "hsl(216, 50%, 16%)",
+          darker: "hsl(217, 54%, 11%)",
+          light: "hsl(215, 32%, 27%)",
+        },
+      },
+      fontFamily: {
+        'body': ['Outfit']
+      },
+      backgroundImage: ['hover', 'focus'],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
