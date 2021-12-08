0f687e539b77b8008a2b4e6c5c4bdbc094ecd895
Agrima <qvistn111@gmail.com>
2021-12-08 13:02:46 +0530

    desktop view

--- tip-calculator-app-main/tailwind.config.js
+++ tip-calculator-app-main/tailwind.config.js
@@ -6,17 +6,18 @@ module.exports = {
     extend: {
       colors: {
         cyan: {
-          Strong: "hsl(172, 67%, 45%)",
+          strong: "hsl(172, 67%, 45%)",
           verydark: "hsl(183, 100%, 15%)",
           darkgrayish: "hsl(186, 14%, 43%)",
-          darkgrayish: "hsl(184, 14%, 56%)",
+          darkgrayish1: "hsl(184, 14%, 56%)",
           lightgrayish: "hsl(185, 41%, 84%)",
           lightgrayish1: "hsl(189, 41%, 97%)",
           white: "hsl(0, 0%, 100%)",
+          lightgray:"hsl(202.5, 50%, 96.9%)",
         },
       },
       fontFamily: {
-        body: ["Barlow Semi Condensed"],
+        body: ["Space Mono"],
       },
       backgroundImage: {
         "icon-person": "url(../images/icon-person.svg)",
