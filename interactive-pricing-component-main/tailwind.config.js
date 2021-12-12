dd359c0f79ebd9ea013d5825be78725a18eb0edc
Agrima <qvistn111@gmail.com>
2021-12-12 11:58:10 +0530

    included font family as well as the colors to be used in this project

--- interactive-pricing-component-main/tailwind.config.js
+++ interactive-pricing-component-main/tailwind.config.js
@@ -6,23 +6,43 @@ module.exports = {
     extend: {
       colors: {
         cyan: {
-          strong: "hsl(172, 67%, 45%)",
-          verydark: "hsl(183, 100%, 15%)",
-          darkgrayish: "hsl(186, 14%, 43%)",
-          darkgrayish1: "hsl(184, 14%, 56%)",
-          lightgrayish: "hsl(185, 41%, 84%)",
-          lightgrayish1: "hsl(189, 41%, 97%)",
-          white: "hsl(0, 0%, 100%)",
-          lightgray:"hsl(202.5, 50%, 96.9%)",
+          //Full Slider Bar
+          soft: "hsl(174, 77%, 80%)",
+          //Slider Backround
+          strong: "hsl(174, 86%, 45%)",
+        },
+        red: {
+          //Discount Background
+          lightGrayish: "hsl(14, 92%, 95%)",
+          //Discount Text
+          light: "hsl(15, 100%, 70%)",
+        },
+        blue: {
+          //CTA Text
+          pale: "hsl(226, 100%, 87%)",
+          //Main Background
+          veryPale: "hsl(230, 100%, 99%)",
+          //Empty Slider Bar
+          lightGrayish: "hsl(224, 65%, 95%)",
+          //Toggle Background
+          mediumGrayish1: "hsl(223, 50%, 87%)",
+          //Text
+          grayish: " hsl(225, 20%, 60%)",
+          //Text & CTA Background
+          darkDesaturated: "hsl(227, 35%, 25%)",
+        },
+        white: {
+          //Pricing Component Background
+          neutral: "hsl (0, 0%, 100%)",
         },
       },
       fontFamily: {
-        body: ["Space Mono"],
+        body: ["Manrope"],
       },
       backgroundImage: {
         "icon-person": "url(../images/icon-person.svg)",
         "icon-dollar": "url(../images/icon-dollar.svg)",
-        "logo": "url(../images/logo.svg)",
+        logo: "url(../images/logo.svg)",
       },
     },
   },
