f2fad5b3276432b3664978607dc8faa0168e47ce
Agrima <qvistn111@gmail.com>
2021-12-13 20:07:49 +0530

    added toggle switch, start my trial button, list of items

--- interactive-pricing-component-main/tailwind.config.js
+++ interactive-pricing-component-main/tailwind.config.js
@@ -33,7 +33,7 @@ module.exports = {
         },
         white: {
           //Pricing Component Background
-          neutral: "hsl (0, 0%, 100%)",
+          neutral: "hsl(0, 0%, 100%)",
         },
       },
       fontFamily: {
@@ -42,8 +42,9 @@ module.exports = {
       backgroundImage: {
         "icon-check": "url(../images/icon-check.svg)",
         "icon-slider": "url(../images/icon-slider.svg)",
-        "bg-pattern": "url(../images/bg-pattern.svg)",
+        "pattern": "url(../images/bg-pattern.svg)",
         "pattern-circles": "url(../images/pattern-circles.svg)",
+        "arrow": "url(../images/arrow.png)",
       },
     },
   },
