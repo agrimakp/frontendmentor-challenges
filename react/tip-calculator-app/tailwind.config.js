cf382129f321943e4d9fccae87c0161d688c5814
Agrima <qvistn111@gmail.com>
2022-01-03 19:05:57 +0530

    updated tailwind config

--- react/tip-calculator-app/tailwind.config.js
+++ react/tip-calculator-app/tailwind.config.js
@@ -3,4 +3,28 @@ module.exports = {
   theme: { extend: {} },
   plugins: [],
   mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        cyan: {
+          strong: "hsl(172, 67%, 45%)",
+          verydark: "hsl(183, 100%, 15%)",
+          darkgrayish: "hsl(186, 14%, 43%)",
+          darkgrayish1: "hsl(184, 14%, 56%)",
+          lightgrayish: "hsl(185, 41%, 84%)",
+          lightgrayish1: "hsl(189, 41%, 97%)",
+          white: "hsl(0, 0%, 100%)",
+          lightgray:"hsl(202.5, 50%, 96.9%)",
+        },
+      },
+      fontFamily: {
+        body: ["Space Mono"],
+      },
+      backgroundImage: {
+        "icon-person": "url(../images/icon-person.svg)",
+        "icon-dollar": "url(../images/icon-dollar.svg)",
+        "logo": "url(../images/logo.svg)",
+      },
+    },
+  },
 };
