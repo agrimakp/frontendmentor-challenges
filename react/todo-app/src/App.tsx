daf031ca709fdee3fe125a2bc4cb49d48697fe96
Agrima <qvistn111@gmail.com>
2022-01-05 12:06:32 +0530

    added logo and fonts

--- react/todo-app/src/App.tsx
+++ react/todo-app/src/App.tsx
@@ -1,11 +1,29 @@
-import { HelloWorld } from "./components/HelloWorld";
+import { HeaderComponent } from "./components/HeaderComponent";
+import { TodoComponent } from "./components/TodoComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HelloWorld />
-      </header>
+    <div
+      className=" h-screen
+    font-body
+    text-[18px] text-white
+    bg-mobile-dark bg-no-repeat bg-blue-veryDark
+    md:bg-desktop-dark
+    bg-contain
+    flex
+    justify-center"
+    >
+      <div
+        className="w-[325px]
+        md:w-[600px]
+        h-[200px]
+        flex flex-col
+        mt-12
+        text-blue-darkGrayish"
+      >
+        <HeaderComponent />
+        <TodoComponent />
+      </div>
     </div>
   );
 }
