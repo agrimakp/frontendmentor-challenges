61930e117c2ced6f732abdf278cdd1e21b4b64f5
Agrima <qvistn111@gmail.com>
2022-01-05 12:04:38 +0530

    Created header component

---
+++ react/todo-app/src/components/HeaderComponent.tsx
@@ -0,0 +1,10 @@
+export function HeaderComponent() {
+  return (
+    <div className="my-4 flex justify-between">
+      <h1 className="text-3xl md:text-5xl text-white font-bold tracking-[12px]">
+        TODO
+      </h1>
+      <div className="bg-icon-sun bg-no-repeat w-8 h-8"></div>
+    </div>
+  );
+}
